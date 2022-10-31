b708931c03eaab3963f6b777bc0e508472109867
log1342 <113910996+log1342@users.noreply.github.com>
2022-10-31 08:46:07 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+function RED () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Red))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+function GREEN () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Green))
+}
+function YELLOW () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+let range: neopixel.Strip = null
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+strip.setBrightness(70)
+basic.forever(function () {
+    RED()
+    basic.pause(2000)
+    GREEN()
+    basic.pause(2000)
+    YELLOW()
+    basic.pause(2000)
+})
